48da2c7744240d7892c27b239e8d133682993a1e
Shawqi Hatem <shawqicauses@outlook.com>
2022-07-08 15:37:20 +0300

    Build: Refactor rendering buttons in sidebar component

--- components/sidebar/index.jsx
+++ components/sidebar/index.jsx
@@ -6,9 +6,67 @@ import {
   RssIcon,
   SearchIcon
 } from "@heroicons/react/outline"
+import { Fragment } from "react"
 import { classes } from "../../lib/utils"
 
 function Sidebar() {
+  const data = [
+    {
+      name: "Home",
+      icon: (
+        <HomeIcon
+          className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+          strokeWidth={1.5}
+        />
+      )
+    },
+    {
+      name: "Search",
+      icon: (
+        <SearchIcon
+          className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+          strokeWidth={1.5}
+        />
+      )
+    },
+    {
+      name: "Your Library",
+      icon: (
+        <LibraryIcon
+          className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+          strokeWidth={1.5}
+        />
+      )
+    },
+    {
+      name: "Create Playlist",
+      icon: (
+        <PlusCircleIcon
+          className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+          strokeWidth={1.5}
+        />
+      )
+    },
+    {
+      name: "Liked Songs",
+      icon: (
+        <HeartIcon
+          className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+          strokeWidth={1.5}
+        />
+      )
+    },
+    {
+      name: "Your Episodes",
+      icon: (
+        <RssIcon
+          className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+          strokeWidth={1.5}
+        />
+      )
+    }
+  ]
+
   return (
     <nav
       className={classes(
@@ -20,79 +78,24 @@ function Sidebar() {
           "flex h-full w-full flex-col",
           "items-start justify-center gap-5"
         )}>
-        <button
-          className={classes(
-            "flex w-full items-center justify-start gap-5",
-            "text-base font-normal text-zinc-500",
-            "truncate transition hover:text-white"
-          )}>
-          <HomeIcon
-            className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
-            strokeWidth={1.5}
-          />
-          <p>Home</p>
-        </button>
-        <button
-          className={classes(
-            "flex w-full items-center justify-start gap-5",
-            "text-base font-normal text-zinc-500",
-            "truncate transition hover:text-white"
-          )}>
-          <SearchIcon
-            className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
-            strokeWidth={1.5}
-          />
-          <p>Search</p>
-        </button>
-        <button
-          className={classes(
-            "flex w-full items-center justify-start gap-5",
-            "text-base font-normal text-zinc-500",
-            "truncate transition hover:text-white"
-          )}>
-          <LibraryIcon
-            className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
-            strokeWidth={1.5}
-          />
-          <p>Your Library</p>
-        </button>
-        <hr className={classes("h-0.5 w-full border-0 bg-zinc-800")} />
-        <button
-          className={classes(
-            "flex w-full items-center justify-start gap-5",
-            "text-base font-normal text-zinc-500",
-            "truncate transition hover:text-white"
-          )}>
-          <PlusCircleIcon
-            className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
-            strokeWidth={1.5}
-          />
-          <p>Create Playlist</p>
-        </button>
-        <button
-          className={classes(
-            "flex w-full items-center justify-start gap-5",
-            "text-base font-normal text-zinc-500",
-            "truncate transition hover:text-white"
-          )}>
-          <HeartIcon
-            className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
-            strokeWidth={1.5}
-          />
-          <p>Liked Songs</p>
-        </button>
-        <button
-          className={classes(
-            "flex w-full items-center justify-start gap-5",
-            "text-base font-normal text-zinc-500",
-            "truncate transition hover:text-white"
-          )}>
-          <RssIcon
-            className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
-            strokeWidth={1.5}
-          />
-          <p>Your Episodes</p>
-        </button>
+        {data.map(({ name, icon }, index) => {
+          return (
+            <Fragment key={index}>
+              <button
+                className={classes(
+                  "flex w-full items-center justify-start gap-5",
+                  "text-base font-normal text-zinc-500",
+                  "truncate transition hover:text-white"
+                )}>
+                {icon}
+                <p>{name}</p>
+              </button>
+              {index === 2 ? (
+                <hr className={classes("h-0.5 w-full border-0 bg-zinc-800")} />
+              ) : null}
+            </Fragment>
+          )
+        })}
       </div>
     </nav>
   )
