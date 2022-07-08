7366b8fef38b95bbb22b59b0eb94ce31d024cc2b
Shawqi Hatem <shawqicauses@outlook.com>
2022-07-08 15:26:42 +0300

    Update: Enhance through adding more sidebar buttons

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,11 @@
-import { HomeIcon, LibraryIcon, SearchIcon } from "@heroicons/react/outline"
+import {
+  HeartIcon,
+  HomeIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  RssIcon,
+  SearchIcon
+} from "@heroicons/react/outline"
 import { signOut } from "next-auth/react"
 import Head from "next/head"
 import { classes } from "../lib/utils"
@@ -63,6 +70,43 @@ function Home() {
                 strokeWidth={1.5}
               />
               <p>Your Library</p>
+            </button>
+            <hr className={classes("h-0.5 w-full border-0 bg-zinc-800")} />
+            <button
+              className={classes(
+                "flex w-full items-center justify-start gap-5",
+                "text-base font-normal text-zinc-500",
+                "truncate transition hover:text-white"
+              )}>
+              <PlusCircleIcon
+                className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+                strokeWidth={1.5}
+              />
+              <p>Create Playlist</p>
+            </button>
+            <button
+              className={classes(
+                "flex w-full items-center justify-start gap-5",
+                "text-base font-normal text-zinc-500",
+                "truncate transition hover:text-white"
+              )}>
+              <HeartIcon
+                className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+                strokeWidth={1.5}
+              />
+              <p>Liked Songs</p>
+            </button>
+            <button
+              className={classes(
+                "flex w-full items-center justify-start gap-5",
+                "text-base font-normal text-zinc-500",
+                "truncate transition hover:text-white"
+              )}>
+              <RssIcon
+                className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+                strokeWidth={1.5}
+              />
+              <p>Your Episodes</p>
             </button>
           </div>
         </nav>
