e855f9b1392615c56f6d18b9709b6312e3b4c2ca
Shawqi Hatem <shawqicauses@outlook.com>
2022-07-08 15:22:17 +0300

    New: Build website's simple sidebar layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { HomeIcon, LibraryIcon, SearchIcon } from "@heroicons/react/outline"
 import { signOut } from "next-auth/react"
 import Head from "next/head"
 import { classes } from "../lib/utils"
@@ -17,6 +18,54 @@ function Home() {
           "container mx-auto px-5",
           "flex flex-col items-center justify-center"
         )}>
+        <nav
+          className={classes(
+            "mb-5 w-full max-w-xs p-6",
+            "rounded-lg border border-zinc-800"
+          )}>
+          <div
+            className={classes(
+              "flex h-full w-full flex-col",
+              "items-start justify-center gap-5"
+            )}>
+            <button
+              className={classes(
+                "flex w-full items-center justify-start gap-5",
+                "text-base font-normal text-zinc-500",
+                "truncate transition hover:text-white"
+              )}>
+              <HomeIcon
+                className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+                strokeWidth={1.5}
+              />
+              <p>Home</p>
+            </button>
+            <button
+              className={classes(
+                "flex w-full items-center justify-start gap-5",
+                "text-base font-normal text-zinc-500",
+                "truncate transition hover:text-white"
+              )}>
+              <SearchIcon
+                className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+                strokeWidth={1.5}
+              />
+              <p>Search</p>
+            </button>
+            <button
+              className={classes(
+                "flex w-full items-center justify-start gap-5",
+                "text-base font-normal text-zinc-500",
+                "truncate transition hover:text-white"
+              )}>
+              <LibraryIcon
+                className={classes("h-6 w-6 stroke-current", "hidden sm:block")}
+                strokeWidth={1.5}
+              />
+              <p>Your Library</p>
+            </button>
+          </div>
+        </nav>
         <button
           onClick={() => signOut()}
           className={classes("button button-dark")}>
